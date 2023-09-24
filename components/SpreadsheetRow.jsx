6c643eb099232ae844830ffbe0db574a1e64cc06
Brian Henning <henninb@msn.com>
2023-09-24 06:59:09 -0500

    wip

--- components/SpreadsheetRow.jsx
+++ components/SpreadsheetRow.jsx
@@ -12,8 +12,27 @@ export default function SpreadsheetRow({ rowData }) {
     setEditableFields({ ...editableFields, [fieldName]: false });
   };
 
-  const handleContentChange = (fieldName, newValue) => {
-    setContent({ ...content, [fieldName]: newValue });
+  const handleContentChange = (fieldName, newValue, dataType) => {
+    if (dataType === 'currency') {
+      // Ensure newValue is numeric for currency field
+      if (!isNaN(newValue)) {
+        setContent({ ...content, [fieldName]: newValue });
+      }
+    } else if (dataType === 'date') {
+      // Ensure newValue is a valid date format (you can customize this validation)
+      if (/^\d{4}-\d{2}-\d{2}$/.test(newValue)) {
+        setContent({ ...content, [fieldName]: newValue });
+      }
+    } else {
+      setContent({ ...content, [fieldName]: newValue });
+    }
+  };
+
+  const handleInputKeyDown = (fieldName, e, dataType) => {
+    if (e.key === 'Enter') {
+      // Commit the change when Enter key is pressed
+      handleFieldBlur(fieldName);
+    }
   };
 
   const formatCurrency = (amount) => {
@@ -23,13 +42,18 @@ export default function SpreadsheetRow({ rowData }) {
     });
   };
 
-  const renderEditableCell = (fieldName, displayValue) => {
+  const transactionStates = ['Pending', 'Completed', 'Canceled'];
+
+  const renderEditableCell = (fieldName, displayValue, dataType) => {
     return editableFields[fieldName] ? (
       <td onBlur={() => handleFieldBlur(fieldName)}>
         <input
-          type="text"
+          type={dataType === 'currency' || dataType === 'date' ? 'text' : 'text'}
           value={content[fieldName]}
-          onChange={(e) => handleContentChange(fieldName, e.target.value)}
+          onChange={(e) =>
+            handleContentChange(fieldName, e.target.value, dataType)
+          }
+          onKeyDown={(e) => handleInputKeyDown(fieldName, e, dataType)}
         />
       </td>
     ) : (
@@ -41,10 +65,10 @@ export default function SpreadsheetRow({ rowData }) {
 
   return (
     <tr>
-      {renderEditableCell('date', rowData.transactionDate)}
+      {renderEditableCell('date', rowData.transactionDate, 'date')}
       {renderEditableCell('description', rowData.description)}
       {renderEditableCell('category', rowData.category)}
-      {renderEditableCell('amount', formatCurrency(rowData.amount))}
+      {renderEditableCell('amount', formatCurrency(rowData.amount), 'currency')}
       {renderEditableCell('state', rowData.transactionState)}
       {renderEditableCell('type', rowData.type)}
       <td>{rowData.reoccurringType}</td>
