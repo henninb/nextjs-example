84cc2f6e99d87550b0b8293def3a3901184293f6
Brian Henning <henninb@msn.com>
2023-09-24 16:29:58 -0500

    wip

--- pages/transactions.jsx
+++ pages/transactions.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Spreadsheet from '../components/SpreadsheetNew';
 
-export default function MySheet () {
+export default function MySheet() {
   const [data, setData] = useState([]); // Your spreadsheet data
   const [selectedRows, setSelectedRows] = useState([]); // Selected rows
   const [currentPage, setCurrentPage] = useState(1);
-  const rowsPerPage = 50;
+  const rowsPerPage = 10; // 10 records per page
 
   // Functions to handle data modification, row selection, and pagination
   useEffect(() => {
@@ -16,10 +16,41 @@ export default function MySheet () {
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
+  // Function to slice the data based on current page and rows per page
+  const paginatedData = data.slice(
+    (currentPage - 1) * rowsPerPage,
+    currentPage * rowsPerPage
+  );
+
+  // Function to handle page change
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  // Calculate total number of pages
+  const totalPages = Math.ceil(data.length / rowsPerPage);
+
   return (
     <div>
-      <Spreadsheet data={data} />
+      <Spreadsheet data={paginatedData} />
       {/* Pagination controls */}
+      <div>
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
-};
+}
