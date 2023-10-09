44f14e26fa1ac8800e5b78c7db35260f020a79d4
Brian Henning <henninb@msn.com>
2023-10-08 19:26:41 -0500

    wip

---
+++ pages/api/transaction/delete.jsx
@@ -0,0 +1,32 @@
+export default async (request, response) => {
+  const apiUrl = 'http://192.168.10.10:8443/transaction/delete';
+
+  try {
+    //console.log(JSON.stringify(request.body));
+    const uuid = request.url.split('/').pop();
+
+    const fetchResponse = await fetch(`apiUrl/$uuid`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      //body: JSON.stringify(request.body),
+    });
+
+    if (!fetchResponse.ok) {
+      console.log('Response is not ok.');
+      const errorMessage = await fetchResponse.text();
+      response.status(400).json({ error: errorMessage });
+      return;
+    }
+
+    const data = await fetchResponse.json();
+    console.log('Data deleted from the database:', data);
+    response.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    response.status(500).json({ error: 'Internal Server Error' });
+  }
+
+  console.log('End of delete.');
+};
